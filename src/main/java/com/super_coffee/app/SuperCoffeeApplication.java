81d226fdd0fda66ad5ca2d453bf2d490ea4ea768
AlexiSkyline <ilegal_sprite@outlook.com>
2022-09-25 23:50:36 -0500

    add LICENSE

--- src/main/java/com/super_coffee/app/SuperCoffeeApplication.java
+++ src/main/java/com/super_coffee/app/SuperCoffeeApplication.java
@@ -1,10 +1,9 @@
 package com.super_coffee.app;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication @Slf4j
+@SpringBootApplication
 public class SuperCoffeeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SuperCoffeeApplication.class, args);
